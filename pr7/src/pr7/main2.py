c1470bcdc6342fe81a5591256f0c1b608845fc38
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-03-02 14:17:37 +0500

    Update main2.py

--- pr7/src/pr7/main2.py
+++ pr7/src/pr7/main2.py
@@ -1,2 +1,20 @@
-from crewai.flow.flow import flow, start, listen
+from crewai.flow.flow import Flow, start, listen
 from litellm import completion
+
+API_KEY = "sk-1234567890"
+
+class CityFunFact(Flow):
+
+
+    @start()
+    def generate_random_city(self):
+        return = completion(
+            model="gemini/gemini-1.5-pro",
+            api_key=API_KEY,
+            messages=[{                
+                "content": "Generate a random city name and return it as a string."
+            }]
+        )
+        print(result['choices'][0]['message']['content'])
+
+
